335d3b489c3fdc8c62a9cff216d30041b23eb0d2
Laaap <vasilynovogordinsky@yandex.ru>
2026-05-30 11:05:10 +0400

    Revert "1st spisok"
    This reverts commit 95236e08eaadadb96cea4d9eb0c7efd121bb918a.

---
+++ index.js
@@ -0,0 +1,94 @@
+let items = [
+  "Сделать проектную работу",
+  "Полить цветы",
+  "Пройти туториал по Реакту",
+  "Сделать фронт для своего проекта",
+  "Прогуляться по улице в солнечный день",
+  "Помыть посуду",
+];
+
+const listElement = document.querySelector(".to-do__list");
+const formElement = document.querySelector(".to-do__form");
+const inputElement = document.querySelector(".to-do__input");
+
+function loadTasks() {
+  const savedTasks = localStorage.getItem('tasks');
+  return savedTasks ? JSON.parse(savedTasks) : items;
+}
+
+function createItem(item) {
+  const template = document.getElementById("to-do__item-template");
+  const clone = template.content.querySelector(".to-do__item").cloneNode(true);
+  const textElement = clone.querySelector(".to-do__item-text");
+  const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
+  const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
+  const editButton = clone.querySelector(".to-do__item-button_type_edit");
+
+  textElement.textContent = item;
+
+  deleteButton.addEventListener('click', () => {
+    clone.remove();
+    const currentTasks = getTasksFromDOM();
+    saveTasks(currentTasks);
+  });
+
+  duplicateButton.addEventListener('click', () => {
+    const itemName = textElement.textContent;
+    const newItem = createItem(itemName);
+    listElement.prepend(newItem);
+    
+    const currentTasks = getTasksFromDOM();
+    saveTasks(currentTasks);
+  });
+
+  editButton.addEventListener('click', () => {
+    textElement.setAttribute('contenteditable', 'true');
+    textElement.focus();
+  });
+
+  textElement.addEventListener('blur', () => {
+    textElement.setAttribute('contenteditable', 'false');
+    const currentTasks = getTasksFromDOM();
+    saveTasks(currentTasks);
+  });
+
+  
+
+  return clone;
+}
+
+function getTasksFromDOM() {
+  const itemsNamesElements = listElement.querySelectorAll('.to-do__item-text');
+  const tasks = [];
+  
+  itemsNamesElements.forEach(element => {
+    tasks.push(element.textContent);
+  });
+  
+  return tasks;
+}
+
+function saveTasks(tasks) {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+items = loadTasks();
+items.forEach(item => {
+  listElement.append(createItem(item));
+});
+
+formElement.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const taskText = inputElement.value.trim();
+  
+  if (taskText) {
+    const newItem = createItem(taskText);
+    listElement.prepend(newItem);
+    
+    const currentTasks = getTasksFromDOM();
+    saveTasks(currentTasks);
+    
+    inputElement.value = '';
+  }
+});
